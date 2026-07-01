395e29b37cc5e200c647cc688f553a7f59f77942
Tyler <thoward222@ivytech.edu>
2026-07-01 15:06:23 -0400

    Building out initial tables

--- code/main.py
+++ code/main.py
@@ -44,20 +44,45 @@ class BookInstance:
             return False
 
 
-def initialize_table(conn: sqlite3.Connection, cursor: sqlite3.Cursor, table: str) -> None:
+def initialize_tables(conn: sqlite3.Connection, cursor: sqlite3.Cursor) -> None:
     try:
         cursor.execute("""
                     CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    first_name TEXT NOT NULL,
-                    last_name TEXT NOT NULL,
-                    street_address TEXT NOT NULL,
-                    city_address TEXT NOT NULL,
-                    state_address TEXT NOT NULL,
-                    zip_address INTEGER NOT NULL,
-                    phone_number INTEGER
-                    )"""
-                    )
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        first_name TEXT NOT NULL,
+                        last_name TEXT NOT NULL,
+                        street_address TEXT NOT NULL,
+                        city_address TEXT NOT NULL,
+                        state_address TEXT NOT NULL,
+                        zip_address INTEGER NOT NULL,
+                        phone_number INTEGER
+                        )
+                    """)
+        
+        cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS genres (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT UNIQUE NOT NULL
+                        )
+                    """)
+
+        cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS books (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        title TEXT NOT NULL,
+                        author TEXT NOT NULL
+                        )
+                    """)
+        
+        cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS book_genres (
+                        book_id INTEGER,
+                        genre_id INTEGER,
+                        PRIMARY KEY (book_id, genre_id),
+                        FOREIGN KEY (book_id) REFERENCES books(id),
+                        FOREIGN KEY (genre_id) REFERENCES genres(id)
+                        )
+                    """)
         
         conn.commit()
         print("Table initialized successfully.")
@@ -68,7 +93,7 @@ def initialize_table(conn: sqlite3.Connection, cursor: sqlite3.Cursor, table: st
         cursor.close()
         conn.close()
 
-def add_user(conn: sqlite3.Connection, cursor: sqlite3.Cursor, table: str, first_name : str, last_name : str, street_address : str, city_address : str, state_address : str, zip_address : int, phone_number : int) -> None:
+def add_user(conn: sqlite3.Connection, cursor: sqlite3.Cursor, user: User) -> None:
     try:
         cursor.execute("""
                        INSERT INTO users (
@@ -81,13 +106,13 @@ def add_user(conn: sqlite3.Connection, cursor: sqlite3.Cursor, table: str, first
                        phone_number
                        ) VALUES (?, ?)
                        """, (
-                           first_name,
-                           last_name,
-                           street_address,
-                           city_address,
-                           state_address,
-                           zip_address,
-                           phone_number
+                           user.first_name,
+                           user.last_name,
+                           user.street_address,
+                           user.city_address,
+                           user.state_address,
+                           user.zip_address,
+                           user.phone_number
                        ))
         
         conn.commit()
